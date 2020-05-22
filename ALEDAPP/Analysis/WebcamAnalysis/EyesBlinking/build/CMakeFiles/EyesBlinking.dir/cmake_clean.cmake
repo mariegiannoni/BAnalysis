331fc820ab4c54ead4dcce2f5645b92b6d41175e
mariegiannoni <52198581+mariegiannoni@users.noreply.github.com>
2020-05-22 21:34:35 +0200

    Add files via upload

---
+++ ALEDAPP/Analysis/WebcamAnalysis/EyesBlinking/build/CMakeFiles/EyesBlinking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EyesBlinking.dir/EyesBlinking.cpp.o"
+  "EyesBlinking.pdb"
+  "EyesBlinking"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EyesBlinking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
