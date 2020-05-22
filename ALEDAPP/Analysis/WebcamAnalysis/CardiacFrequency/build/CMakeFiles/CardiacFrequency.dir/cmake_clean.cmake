480e5f571ee3d5d37ca11b1f01a6095506d0ab0f
mariegiannoni <52198581+mariegiannoni@users.noreply.github.com>
2020-05-22 21:19:04 +0200

    Add files via upload

---
+++ ALEDAPP/Analysis/WebcamAnalysis/CardiacFrequency/build/CMakeFiles/CardiacFrequency.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CardiacFrequency.dir/CardiacFrequency.cpp.o"
+  "CardiacFrequency.pdb"
+  "CardiacFrequency"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CardiacFrequency.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
